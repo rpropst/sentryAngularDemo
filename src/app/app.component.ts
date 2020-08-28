bf18de5c2255573be4370799dbbfe341c6924d68
Richard Propst <richard.propst@gmail.com>
2020-08-28 12:37:32 -0700

    initial angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {throwError} from "rxjs/index";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .then(success => console.log(`Bootstrap success`))
+//   .catch(err => console.error(err));
+
 export class AppComponent {
   title = 'sentryDemo';
+
+  onClickMe() {
+    console.error('Test error');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,18 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+
+import * as Sentry from '@sentry/angular';
+
+Sentry.init({
+  dsn: 'https://307f280cdb2b4561bdd989d029b573a7@o439594.ingest.sentry.io/5408425'
+});
 
 import { AppComponent } from './app.component';
+
+enableProdMode();
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,19 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true
+      })
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(err => console.error(err));
